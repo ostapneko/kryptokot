b08a73c88e1c6e5ed734d1da40c88e3fc3159fea
Emmanuelle Poirier <manuepoirier@gmail.com>
2016-08-03 15:47:18 +0100

    Add executable for challenge 1.3

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
 import Lib
+import System.Environment
+import Hex
+import Xor
+import qualified Data.ByteString.Char8 as C8
 
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  case args of
+    ["1", "3"] -> run13
+    _ -> error "Unknown challenge"
+
+run13 :: IO ()
+run13 = do
+  let msg = HexString "1B37373331363F78151B7F2B783431333D78397828372D363C78373E783A393B3736"
+      bsMsg = toByteString msg
+      key = findBestKey  bsMsg
+      dec = xorSingle key bsMsg
+  C8.putStrLn dec
